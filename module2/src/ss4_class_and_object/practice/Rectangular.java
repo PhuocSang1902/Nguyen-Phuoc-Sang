de386cdb2503d8d14072193dc0a8793a3d286542
PhuocSang1902 <phuocsang1902@gmail.com>
2022-09-08 18:11:41 +0700

    submit method

---
+++ module2/src/ss4_class_and_object/practice/Rectangular.java
@@ -0,0 +1,28 @@
+package ss4_class_and_object.practice;
+
+public class Rectangular {
+    private double width;
+    private double height;
+
+    Rectangular(double width, double height){
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+}
+
