79eb0a68345994f32214ab2cfb47803c7921c598
PhuocSang1902 <phuocsang1902@gmail.com>
2022-10-14 11:13:21 +0700

    submit ss1 practice and exercise

---
+++ module3/ss1_database/practice/lesson1_create_database_with_mysql_workbrench.sql
@@ -0,0 +1,14 @@
+create database my_database;
+use my_database;
+create table my_table(
+id int,
+`name` varchar(50),
+age int
+);
+create table my_table2(
+id int,
+`name` varchar(50),
+age int
+);
+drop table my_table;
+drop table my_table2;
