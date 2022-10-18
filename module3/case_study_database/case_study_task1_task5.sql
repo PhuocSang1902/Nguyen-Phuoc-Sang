ce731a6477a1fbb837f2275a51a119217adeb288
PhuocSang1902 <phuocsang1902@gmail.com>
2022-10-18 16:21:33 +0700

    update

--- module3/case_study_database/case_study_task1_task5.sql
+++ module3/case_study_database/case_study_task1_task5.sql
@@ -48,7 +48,7 @@ SELECT
 FROM
 	hop_dong hd JOIN dich_vu dv ON hd.ma_dich_vu = dv.ma_dich_vu 
      LEFT JOIN hop_dong_chi_tiet hdct ON hd.ma_hop_dong = hdct.ma_hop_dong
-     LEFT JOIN dich_vu_di_kem dvdk ON hdct.ma_dich_vu_di_kem
+     LEFT JOIN dich_vu_di_kem dvdk ON hdct.ma_dich_vu_di_kem = dvdk.ma_dich_vu_di_kem
 GROUP BY
 	hd.ma_hop_dong;
     
