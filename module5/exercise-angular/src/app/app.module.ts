db5bd4af1673f37af9b4ffa9f42d20a8744590f7
PhuocSang1902 <110214702+PhuocSang1902@users.noreply.github.com>
2022-12-25 09:13:16 +0700

    update

--- module5/exercise-angular/src/app/app.module.ts
+++ module5/exercise-angular/src/app/app.module.ts
@@ -5,17 +5,27 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Calculator1Component } from './calculator1/calculator1.component';
 import { ColorPickComponent } from './color-pick/color-pick.component';
+import { ArticleComponent } from './hackernews/article/article.component';
+import {FormsModule} from '@angular/forms';
+import { LikeComponent } from './hackernews/like/like.component';
+import { NavbarComponent } from './hackernews/navbar/navbar.component';
+import { FooterComponent } from './hackernews/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Calculator1Component,
-    ColorPickComponent
+    ColorPickComponent,
+    ArticleComponent,
+    LikeComponent,
+    NavbarComponent,
+    FooterComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
