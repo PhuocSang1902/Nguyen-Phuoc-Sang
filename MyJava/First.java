7ba479fa796bccd8ffeae517ff579bac811a9f91
PhuocSang1902 <110214702+PhuocSang1902@users.noreply.github.com>
2023-04-20 08:29:36 +0700

    add file First in MyJava

--- MyJava/First.java
+++ MyJava/First.java
@@ -1,6 +1,10 @@
 
 public class First{
 	public static void main(String[] args){
-		System.out.print("Hello World");
+		System.out.print("Hello World ");
+		for(int i = 0; i  <  args.length; i++){
+			System.out.print(args[i] + " ");
+		}
+		
 	}
 }
