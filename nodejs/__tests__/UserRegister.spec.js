3b4849824df7d1efc915a0397d404782f471de2b
Nguyen Phuoc Sang <nguyen.phuoc.sang@dmtholding.com>
2023-08-22 16:18:49 +0700

    update

--- nodejs/__tests__/UserRegister.spec.js
+++ nodejs/__tests__/UserRegister.spec.js
@@ -1,10 +1,20 @@
 const request = require('supertest');
-const app = require('../app');
+const app = require('../src/app');
+const User = require('../src/user/User');
+const sequelize = require('../src/config/database');
+
+beforeAll(() => {
+  return sequelize.sync();
+});
+
+beforeEach(() => {
+  return User.destroy({ truncate: true });
+});
 
 describe('User Registration', () => {
   it('returns 200 ok when signup request is valid', (done) => {
     request(app)
-      .post('/api/1.0/user')
+      .post('/api/1.0/users')
       .send({
         username: 'user1',
         email: 'user1@gmail.com',
@@ -18,7 +28,7 @@ describe('User Registration', () => {
 
   it('returns success message when signup request is valid', (done) => {
     request(app)
-      .post('/api/1.0/user')
+      .post('/api/1.0/users')
       .send({
         username: 'user1',
         email: 'user1@gmail.com',
@@ -29,4 +39,38 @@ describe('User Registration', () => {
         done();
       });
   });
+});
+
+it('saves the user to database', (done) => {
+  request(app)
+    .post('/api/1.0/users')
+    .send({
+      username: 'user1',
+      email: 'user1@gmail.com',
+      password: 'password',
+    })
+    .then(() => {
+      User.findAll().then((userList) => {
+        expect(userList.length).toBe(1);
+        done();
+      });
+    });
+});
+
+it('saves the username and email to database', (done) => {
+  request(app)
+    .post('/api/1.0/users')
+    .send({
+      username: 'user1',
+      email: 'user1@gmail.com',
+      password: 'password',
+    })
+    .then(() => {
+      User.findAll().then((userList) => {
+        const savedUser = userList[0];
+        expect(savedUser.username).toBe('user1');
+        expect(savedUser.email).toBe('user1@gmail.com');
+        done();
+      });
+    });
 });
