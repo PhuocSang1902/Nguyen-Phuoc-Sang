17a7929e0ade9212fe1cf0948e6cbe79c4681d17
Nguyen Phuoc Sang <nguyen.phuoc.sang@dmtholding.com>
2023-08-24 15:14:20 +0700

    update

--- nodejs/__tests__/UserRegister.spec.js
+++ nodejs/__tests__/UserRegister.spec.js
@@ -11,33 +11,34 @@ beforeEach(() => {
   return User.destroy({ truncate: true });
 });
 
-describe('User Registration', () => {
-  const postValidUser = () => {
-    return request(app).post('/api/1.0/users').send({
-      username: 'user1',
-      email: 'user1@gmail.com',
-      password: 'password',
-    });
-  };
+const validUser = {
+  username: 'user1',
+  email: 'user1@gmail.com',
+  password: 'password',
+};
+function postUser(user = validUser) {
+  return request(app).post('/api/1.0/users').send(user);
+}
 
+describe('User Registration', () => {
   it('returns 200 ok when signup request is valid', async () => {
-    const response = await postValidUser();
+    const response = await postUser();
     expect(response.status).toBe(200);
   });
 
   it('returns success message when signup request is valid', async () => {
-    const response = await postValidUser();
+    const response = await postUser();
     expect(response.body.message).toBe('User created');
   });
 
   it('saves the user to database', async () => {
-    await postValidUser();
+    await postUser();
     const userList = await User.findAll();
     expect(userList.length).toBe(1);
   });
 
   it('saves the username and email to database', async () => {
-    await postValidUser();
+    await postUser();
     const userList = await User.findAll();
     const savedUser = userList[0];
     expect(savedUser.username).toBe('user1');
@@ -45,9 +46,55 @@ describe('User Registration', () => {
   });
 
   it('hashes the password in database', async () => {
-    await postValidUser();
+    await postUser();
     const userList = await User.findAll();
     const savedUser = userList[0];
     expect(savedUser.password).not.toBe('password');
+  });
+
+  it('return 400 when username is null', async () => {
+    const response = await postUser({
+      username: null,
+      email: 'user1@gmail.com',
+      password: 'password',
+    });
+    expect(response.status).toBe(400);
+  });
+
+  it('return validationErrors field in response when validation error occurs', async () => {
+    const response = await request(app).post('/api/1.0/users').send({
+      username: null,
+      email: 'user1@gmail.com',
+      password: 'password',
+    });
+    const body = response.body;
+    expect(body.validationErrors).not.toBeUndefined();
+  });
+
+  it('return errors for both when username and email is null', async () => {
+    const response = await postUser({
+      username: null,
+      email: null,
+      password: 'password',
+    });
+    const body = response.body;
+    expect(Object.keys(body.validationErrors)).toEqual(['username', 'email']);
+  });
+
+  it.each`
+    field         | expectedMessage
+    ${'username'} | ${'Username cannot be null'}
+    ${'email'}    | ${'E-mail cannot be null'}
+    ${'password'} | ${'Password cannot be null'}
+  `('return $expectedMessage when $field is null', async ({ field, expectedMessage }) => {
+    const user = {
+      username: 'user1',
+      email: 'user1@gmail.com',
+      password: 'password',
+    };
+    user[field] = null;
+    const response = await postUser(user);
+    const body = response.body;
+    expect(body.validationErrors[field]).toBe(expectedMessage);
   });
 });
--- nodejs/src/user/UserRouter.js
+++ nodejs/src/user/UserRouter.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const UserService = require('./UserService');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 
-router.post('/api/1.0/users', async (req, res) => {
-  await UserService.save(req.body);
-  return res.send({ message: 'User created' });
-});
+router.post(
+  '/api/1.0/users',
+  check('username').notEmpty().withMessage('Username cannot be null'),
+  check('email').notEmpty().withMessage('E-mail cannot be null'),
+  check('password').notEmpty().withMessage('Password cannot be null'),
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const validationErrors = {};
+      errors.array().forEach((element) => {
+        validationErrors[element.path] = element.msg;
+      });
+      return res.status(400).send({ validationErrors: validationErrors });
+    }
+    await UserService.save(req.body);
+    return res.send({ message: 'User created' });
+  },
+);
 
 module.exports = router;
