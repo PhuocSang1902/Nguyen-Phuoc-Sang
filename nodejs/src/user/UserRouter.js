465b64b9666de52ea736e0e87e962ee137eecac9
PhuocSang1902 <110214702+PhuocSang1902@users.noreply.github.com>
2023-09-07 08:56:02 +0700

    update

--- nodejs/src/user/UserRouter.js
+++ nodejs/src/user/UserRouter.js
@@ -65,12 +65,8 @@ router.post('/api/1.0/users/token/:token', async (req, res, next) => {
 });
 
 router.get('/api/1.0/users', async (req, res, next) => {
-  res.send({
-    content: [],
-    page: 0,
-    size: 10,
-    totalPages: 0,
-  });
+  const users = await UserService.getUsers();
+  res.send(users);
 });
 
 module.exports = router;
