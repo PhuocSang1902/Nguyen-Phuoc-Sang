7749bc75aa51f07f28eacb61c9ac355ff1a8ef75
Nguyen.Phuoc.Sang <phuocsang1902@gmail.com>
2024-11-08 18:44:23 +0700

    Update

--- comps/src/pages/ModalPage.js
+++ comps/src/pages/ModalPage.js
@@ -9,12 +9,246 @@ function ModalPage() {
     setShowModal(true);
   };
 
+  const handleClose = () => {
+    setShowModal(false);
+  };
+
+  const actionBar = (
+    <div>
+      <Button primary onClick={handleClose}>
+        I accept
+      </Button>
+    </div>
+  );
+
+  const modal = (
+    <Modal onClose={handleClose} actionBar={actionBar}>
+      <p>Here is an important agreement for you to accept</p>
+    </Modal>
+  );
+
   return (
     <div>
       <Button onClick={handleClick} primary>
         Open modal
       </Button>
-      {showModal && <Modal />}
+      {showModal && modal}
+      <p>
+        dkflajlsdkfslfdsafdsafdsafds dkflajlsdkfslfdsafdsafdsafdssdfas
+        dkflajlsdkfslfdsafdsafdsafds{' '}
+      </p>
+      <p>
+        dkflajlsdkfslfdsafdsafdsafds dkflajlsdkfslfdsafdsafdsafdssdfas
+        dkflajlsdkfslfdsafdsafdsafds{' '}
+      </p>
+      <p>
+        dkflajlsdkfslfdsafdsafdsafds dkflajlsdkfslfdsafdsafdsafdssdfas
+        dkflajlsdkfslfdsafdsafdsafds{' '}
+      </p>
+      <p>
+        dkflajlsdkfslfdsafdsafdsafds dkflajlsdkfslfdsafdsafdsafdssdfas
+        dkflajlsdkfslfdsafdsafdsafds{' '}
+      </p>
+      <p>
+        dkflajlsdkfslfdsafdsafdsafds dkflajlsdkfslfdsafdsafdsafdssdfas
+        dkflajlsdkfslfdsafdsafdsafds{' '}
+      </p>
+      <p>
+        dkflajlsdkfslfdsafdsafdsafds dkflajlsdkfslfdsafdsafdsafdssdfas
+        dkflajlsdkfslfdsafdsafdsafds{' '}
+      </p>
+      <p>
+        dkflajlsdkfslfdsafdsafdsafds dkflajlsdkfslfdsafdsafdsafdssdfas
+        dkflajlsdkfslfdsafdsafdsafds{' '}
+      </p>
+      <p>
+        dkflajlsdkfslfdsafdsafdsafds dkflajlsdkfslfdsafdsafdsafdssdfas
+        dkflajlsdkfslfdsafdsafdsafds{' '}
+      </p>
+      <p>
+        dkflajlsdkfslfdsafdsafdsafds dkflajlsdkfslfdsafdsafdsafdssdfas
+        dkflajlsdkfslfdsafdsafdsafds{' '}
+      </p>
+      <p>
+        dkflajlsdkfslfdsafdsafdsafds dkflajlsdkfslfdsafdsafdsafdssdfas
+        dkflajlsdkfslfdsafdsafdsafds{' '}
+      </p>
+      <p>
+        dkflajlsdkfslfdsafdsafdsafds dkflajlsdkfslfdsafdsafdsafdssdfas
+        dkflajlsdkfslfdsafdsafdsafds{' '}
+      </p>
+      <p>
+        dkflajlsdkfslfdsafdsafdsafds dkflajlsdkfslfdsafdsafdsafdssdfas
+        dkflajlsdkfslfdsafdsafdsafds{' '}
+      </p>
+      <p>
+        dkflajlsdkfslfdsafdsafdsafds dkflajlsdkfslfdsafdsafdsafdssdfas
+        dkflajlsdkfslfdsafdsafdsafds{' '}
+      </p>
+      <p>
+        dkflajlsdkfslfdsafdsafdsafds dkflajlsdkfslfdsafdsafdsafdssdfas
+        dkflajlsdkfslfdsafdsafdsafds{' '}
+      </p>
+      <p>
+        dkflajlsdkfslfdsafdsafdsafds dkflajlsdkfslfdsafdsafdsafdssdfas
+        dkflajlsdkfslfdsafdsafdsafds{' '}
+      </p>
+      <p>
+        dkflajlsdkfslfdsafdsafdsafds dkflajlsdkfslfdsafdsafdsafdssdfas
+        dkflajlsdkfslfdsafdsafdsafds{' '}
+      </p>
+      <p>
+        dkflajlsdkfslfdsafdsafdsafds dkflajlsdkfslfdsafdsafdsafdssdfas
+        dkflajlsdkfslfdsafdsafdsafds{' '}
+      </p>
+      <p>
+        dkflajlsdkfslfdsafdsafdsafds dkflajlsdkfslfdsafdsafdsafdssdfas
+        dkflajlsdkfslfdsafdsafdsafds{' '}
+      </p>
+      <p>
+        dkflajlsdkfslfdsafdsafdsafds dkflajlsdkfslfdsafdsafdsafdssdfas
+        dkflajlsdkfslfdsafdsafdsafds{' '}
+      </p>
+      <p>
+        dkflajlsdkfslfdsafdsafdsafds dkflajlsdkfslfdsafdsafdsafdssdfas
+        dkflajlsdkfslfdsafdsafdsafds{' '}
+      </p>
+      <p>
+        dkflajlsdkfslfdsafdsafdsafds dkflajlsdkfslfdsafdsafdsafdssdfas
+        dkflajlsdkfslfdsafdsafdsafds{' '}
+      </p>
+      <p>
+        dkflajlsdkfslfdsafdsafdsafds dkflajlsdkfslfdsafdsafdsafdssdfas
+        dkflajlsdkfslfdsafdsafdsafds{' '}
+      </p>
+      <p>
+        dkflajlsdkfslfdsafdsafdsafds dkflajlsdkfslfdsafdsafdsafdssdfas
+        dkflajlsdkfslfdsafdsafdsafds{' '}
+      </p>
+      <p>
+        dkflajlsdkfslfdsafdsafdsafds dkflajlsdkfslfdsafdsafdsafdssdfas
+        dkflajlsdkfslfdsafdsafdsafds{' '}
+      </p>
+      <p>
+        dkflajlsdkfslfdsafdsafdsafds dkflajlsdkfslfdsafdsafdsafdssdfas
+        dkflajlsdkfslfdsafdsafdsafds{' '}
+      </p>
+      <p>
+        dkflajlsdkfslfdsafdsafdsafds dkflajlsdkfslfdsafdsafdsafdssdfas
+        dkflajlsdkfslfdsafdsafdsafds{' '}
+      </p>
+      <p>
+        dkflajlsdkfslfdsafdsafdsafds dkflajlsdkfslfdsafdsafdsafdssdfas
+        dkflajlsdkfslfdsafdsafdsafds{' '}
+      </p>
+      <p>
+        dkflajlsdkfslfdsafdsafdsafds dkflajlsdkfslfdsafdsafdsafdssdfas
+        dkflajlsdkfslfdsafdsafdsafds{' '}
+      </p>
+      <p>
+        dkflajlsdkfslfdsafdsafdsafds dkflajlsdkfslfdsafdsafdsafdssdfas
+        dkflajlsdkfslfdsafdsafdsafds{' '}
+      </p>
+      <p>
+        dkflajlsdkfslfdsafdsafdsafds dkflajlsdkfslfdsafdsafdsafdssdfas
+        dkflajlsdkfslfdsafdsafdsafds{' '}
+      </p>
+      <p>
+        dkflajlsdkfslfdsafdsafdsafds dkflajlsdkfslfdsafdsafdsafdssdfas
+        dkflajlsdkfslfdsafdsafdsafds{' '}
+      </p>
+      <p>
+        dkflajlsdkfslfdsafdsafdsafds dkflajlsdkfslfdsafdsafdsafdssdfas
+        dkflajlsdkfslfdsafdsafdsafds{' '}
+      </p>
+      <p>
+        dkflajlsdkfslfdsafdsafdsafds dkflajlsdkfslfdsafdsafdsafdssdfas
+        dkflajlsdkfslfdsafdsafdsafds{' '}
+      </p>
+      <p>
+        dkflajlsdkfslfdsafdsafdsafds dkflajlsdkfslfdsafdsafdsafdssdfas
+        dkflajlsdkfslfdsafdsafdsafds{' '}
+      </p>
+      <p>
+        dkflajlsdkfslfdsafdsafdsafds dkflajlsdkfslfdsafdsafdsafdssdfas
+        dkflajlsdkfslfdsafdsafdsafds{' '}
+      </p>
+      <p>
+        dkflajlsdkfslfdsafdsafdsafds dkflajlsdkfslfdsafdsafdsafdssdfas
+        dkflajlsdkfslfdsafdsafdsafds{' '}
+      </p>
+      <p>
+        dkflajlsdkfslfdsafdsafdsafds dkflajlsdkfslfdsafdsafdsafdssdfas
+        dkflajlsdkfslfdsafdsafdsafds{' '}
+      </p>
+      <p>
+        dkflajlsdkfslfdsafdsafdsafds dkflajlsdkfslfdsafdsafdsafdssdfas
+        dkflajlsdkfslfdsafdsafdsafds{' '}
+      </p>
+      <p>
+        dkflajlsdkfslfdsafdsafdsafds dkflajlsdkfslfdsafdsafdsafdssdfas
+        dkflajlsdkfslfdsafdsafdsafds{' '}
+      </p>
+      <p>
+        dkflajlsdkfslfdsafdsafdsafds dkflajlsdkfslfdsafdsafdsafdssdfas
+        dkflajlsdkfslfdsafdsafdsafds{' '}
+      </p>
+      <p>
+        dkflajlsdkfslfdsafdsafdsafds dkflajlsdkfslfdsafdsafdsafdssdfas
+        dkflajlsdkfslfdsafdsafdsafds{' '}
+      </p>
+      <p>
+        dkflajlsdkfslfdsafdsafdsafds dkflajlsdkfslfdsafdsafdsafdssdfas
+        dkflajlsdkfslfdsafdsafdsafds{' '}
+      </p>
+      <p>
+        dkflajlsdkfslfdsafdsafdsafds dkflajlsdkfslfdsafdsafdsafdssdfas
+        dkflajlsdkfslfdsafdsafdsafds{' '}
+      </p>
+      <p>
+        dkflajlsdkfslfdsafdsafdsafds dkflajlsdkfslfdsafdsafdsafdssdfas
+        dkflajlsdkfslfdsafdsafdsafds{' '}
+      </p>
+      <p>
+        dkflajlsdkfslfdsafdsafdsafds dkflajlsdkfslfdsafdsafdsafdssdfas
+        dkflajlsdkfslfdsafdsafdsafds{' '}
+      </p>
+      <p>
+        dkflajlsdkfslfdsafdsafdsafds dkflajlsdkfslfdsafdsafdsafdssdfas
+        dkflajlsdkfslfdsafdsafdsafds{' '}
+      </p>
+      <p>
+        dkflajlsdkfslfdsafdsafdsafds dkflajlsdkfslfdsafdsafdsafdssdfas
+        dkflajlsdkfslfdsafdsafdsafds{' '}
+      </p>
+      <p>
+        dkflajlsdkfslfdsafdsafdsafds dkflajlsdkfslfdsafdsafdsafdssdfas
+        dkflajlsdkfslfdsafdsafdsafds{' '}
+      </p>
+      <p>
+        dkflajlsdkfslfdsafdsafdsafds dkflajlsdkfslfdsafdsafdsafdssdfas
+        dkflajlsdkfslfdsafdsafdsafds{' '}
+      </p>
+      <p>
+        dkflajlsdkfslfdsafdsafdsafds dkflajlsdkfslfdsafdsafdsafdssdfas
+        dkflajlsdkfslfdsafdsafdsafds{' '}
+      </p>
+      <p>
+        dkflajlsdkfslfdsafdsafdsafds dkflajlsdkfslfdsafdsafdsafdssdfas
+        dkflajlsdkfslfdsafdsafdsafds{' '}
+      </p>
+      <p>
+        dkflajlsdkfslfdsafdsafdsafds dkflajlsdkfslfdsafdsafdsafdssdfas
+        dkflajlsdkfslfdsafdsafdsafds{' '}
+      </p>
+      <p>
+        dkflajlsdkfslfdsafdsafdsafds dkflajlsdkfslfdsafdsafdsafdssdfas
+        dkflajlsdkfslfdsafdsafdsafds{' '}
+      </p>
+      <p>
+        dkflajlsdkfslfdsafdsafdsafds dkflajlsdkfslfdsafdsafdsafdssdfas
+        dkflajlsdkfslfdsafdsafdsafds{' '}
+      </p>
     </div>
   );
 }
