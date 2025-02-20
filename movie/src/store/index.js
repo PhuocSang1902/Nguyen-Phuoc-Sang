99b769c4fdaaf03760ad85eb4f4099325cc4fed7
Nguyen.Phuoc.Sang <phuocsang1902@gmail.com>
2025-02-20 19:02:56 +0700

    update

--- movie/src/store/index.js
+++ movie/src/store/index.js
@@ -1,13 +1,48 @@
 import { configureStore, createSlice } from '@reduxjs/toolkit';
 
 const movieSlice = createSlice({
-  name: 'movies',
+  name: 'movie',
   initialState: [],
-  reducers: {},
+  reducers: {
+    addMovie(state, action) {
+      state.push(action.payload);
+    },
+    removeMovie(state, action) {
+      const index = state.indexOf(action.payload);
+      state.splice(index, 1);
+    },
+    reset(state, action) {
+      return [];
+    },
+  },
+});
+
+const songsSlice = createSlice({
+  name: 'song',
+  initialState: [],
+  reducers: {
+    addSong(state, action) {
+      state.push(action.payload);
+    },
+    removeSong(state, action) {
+      const index = state.indexOf(action.payload);
+      state.splice(index, 1);
+    },
+    reset(state, action) {
+      return [];
+    },
+  },
 });
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    songs: songsSlice.reducer,
+    movies: movieSlice.reducer,
+  },
 });
 
-export default store;
+store.dispatch(songsSlice.actions.addSong('New song !!!'));
+
+export { store };
+export const { addSong, removeSong } = songsSlice.actions;
+export const { addMovie, removeMovie, reset } = movieSlice.actions;
