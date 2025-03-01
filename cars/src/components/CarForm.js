17073adb0426b1d0b6aa1d3d26aa9c70b7982e84
Nguyen.Phuoc.Sang <phuocsang1902@gmail.com>
2025-03-01 16:03:22 +0700

    Update

--- cars/src/components/CarForm.js
+++ cars/src/components/CarForm.js
@@ -1,5 +1,25 @@
 function CarForm() {
-  return <div>CarForm</div>;
+ const handleNameChange = (event) => {
+  
+ };
+
+  return (
+    <div className="car-form-panel">
+      <h4 className="subtitle is-3">Add car</h4>
+      <form>
+        <div className="filed-group">
+          <div className="filed">
+            <label className="label">Name</label>
+            <input
+              className="input is-expanded"
+              value={name}
+              onChange={handleNameChange}
+            />
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default CarForm;
