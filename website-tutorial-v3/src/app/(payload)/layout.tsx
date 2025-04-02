1e0c91326e9ca3d73c0db8e9ee485812048c5338
Nguyen.Phuoc.Sang <phuocsang1902@gmail.com>
2025-04-02 15:34:37 +0700

    Update

--- website-tutorial-v3/src/app/(payload)/layout.tsx
+++ website-tutorial-v3/src/app/(payload)/layout.tsx
@@ -6,6 +6,8 @@ import type { ServerFunctionClient } from 'payload'
 import { handleServerFunctions, RootLayout } from '@payloadcms/next/layouts'
 import React from 'react'
 
+import '../styles/payloadStyle.css'
+
 import { importMap } from './admin/importMap.js'
 import './custom.scss'
 
