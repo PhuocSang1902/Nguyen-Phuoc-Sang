49325a0e73e1e92a06163f65305b2534a9bcfa2e
Nguyen.Phuoc.Sang <phuocsang1902@gmail.com>
2025-04-04 10:34:19 +0700

    Add dummy news data, enhance layout with MainHeader, and implement Not Found page. Update News and News Details pages to display news items dynamically.

--- 01-starting-project/app/news/[slug]/page.jsx
+++ 01-starting-project/app/news/[slug]/page.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import { DUMMY_NEWS } from '@/dummy-news';
+import { notFound } from 'next/navigation';
 
 export default function NewsDetailsPage({ params }) {
-  const newsId = params.slug;
+  const newsSlug = params.slug;
+  const newsItem = DUMMY_NEWS.find((news) => news.slug === newsSlug);
+
+  if (!newsItem) {
+    notFound();
+  }
+
   return (
-    <div>
-      <p>News Details Page</p>
-      <p>News id: {newsId}</p>
-    </div>
+    <article className="news-article">
+      <header>
+        <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+        <h1>{newsItem.title}</h1>
+        <time dateTime={newsItem.date}>{newsItem.date}</time>
+      </header>
+      <p>{newsItem.content}</p>
+    </article>
   );
 }
--- 01-starting-project/app/news/page.jsx
+++ 01-starting-project/app/news/page.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import Link from 'next/link';
+import { DUMMY_NEWS } from '@/dummy-news';
 
 export default function NewsPage() {
   return (
     <>
-      <div>News Page</div>
-      <div>
-        <Link href="/news/first-news-item">First News Item</Link>
-      </div>
-      <div>
-        <Link href="/news/second-news-item">Second News Item</Link>
-      </div>
-      <div>
-        <Link href="/news/third-news-item">Third News Item</Link>
-      </div>
+      <h1>News Page</h1>
+      <ul className="news-list">
+        {DUMMY_NEWS.map((newsItem) => (
+          <li key={newsItem.id}>
+            <Link href={`/news/${newsItem.slug}`}>
+              <img
+                src={`/images/news/${newsItem.image}`}
+                alt={newsItem.title}
+              />
+              <span>{newsItem.title}</span>
+            </Link>
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
