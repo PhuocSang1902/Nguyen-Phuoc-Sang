b32e756a8d08fc70a43d2834efb88efa1ec2d7ba
Nguyen.Phuoc.Sang <phuocsang1902@gmail.com>
2025-04-23 17:02:31 +0700

    Implement FilterHeader component for dynamic navigation in FilteredNewsPage, enhancing filter validation and link generation. Replace inline logic with a dedicated component for improved readability and maintainability.

--- 01-starting-project/app/(content)/archive/@archive/[[...filter]]/page.js
+++ 01-starting-project/app/(content)/archive/@archive/[[...filter]]/page.js
@@ -8,6 +8,43 @@ import {
 import Link from 'next/link';
 import { Suspense } from 'react';
 
+async function FilterHeader({ year, month }) {
+  const availableYears = await getAvailableNewsYears();
+
+  let links = availableYears;
+
+  if (
+    (year && !availableYears.includes(year)) ||
+    (month && !getAvailableNewsMonths(year).includes(month))
+  ) {
+    throw new Error('Invalid filter');
+  }
+
+  if (year && !month) {
+    links = getAvailableNewsMonths(year);
+  }
+
+  if (year && month) {
+    links = [];
+  }
+  return (
+    <header id="archive-header">
+      <nav>
+        <ul>
+          {links.map((link) => {
+            const href = year ? `/archive/${year}/${link}` : `/archive/${link}`;
+            return (
+              <li key={link}>
+                <Link href={href}>{link}</Link>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
 async function FilteredNews({ year, month }) {
   let news;
   if (year && !month) {
@@ -31,43 +68,11 @@ export default async function FilteredNewsPage({ params }) {
   const selectedYear = filter?.[0];
   const selectedMonth = filter?.[1];
 
-  const availableYears = await getAvailableNewsYears();
-
-  let links = availableYears;
-
-  if (selectedYear && !selectedMonth) {
-    links = getAvailableNewsMonths(selectedYear);
-  }
-
-  if (selectedYear && selectedMonth) {
-    links = [];
-  }
-
-  if (
-    (selectedYear && !availableYears.includes(selectedYear)) ||
-    (selectedMonth &&
-      !getAvailableNewsMonths(selectedYear).includes(selectedMonth))
-  ) {
-    throw new Error('Invalid filter');
-  }
   return (
     <>
-      <header id="archive-header">
-        <nav>
-          <ul>
-            {links.map((link) => {
-              const href = selectedYear
-                ? `/archive/${selectedYear}/${link}`
-                : `/archive/${link}`;
-              return (
-                <li key={link}>
-                  <Link href={href}>{link}</Link>
-                </li>
-              );
-            })}
-          </ul>
-        </nav>
-      </header>
+      <Suspense fallback={<p>Loading...</p>}>
+        <FilterHeader year={selectedYear} month={selectedMonth} />
+      </Suspense>
       <Suspense fallback={<p>Loading...</p>}>
         <FilteredNews year={selectedYear} month={selectedMonth} />
       </Suspense>
