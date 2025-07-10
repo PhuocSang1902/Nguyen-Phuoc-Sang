661f5f0af6d8c175bd22452e9279e30d9942ebc4
Phuoc.Sang.Nguyen <110214702+PhuocSang1902@users.noreply.github.com>
2025-07-10 23:06:27 +0700

    Add App.vue component to display a list of friends and update main.js to mount the App component

---
+++ vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <section>
+    <h2>My Friends</h2>
+    <ul>
+      <li v-for="friend in friend" :key="friend.id">{{ friend.name }}</li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friend: [
+        {
+          id: 'manuel',
+          name: 'Manuel Lorenz',
+          phone: '0176 34578901',
+          email: 'manuel@localhost.com',
+        },
+        {
+          id: 'julie',
+          name: 'Julie Jones',
+          phone: '0987 654321',
+          email: 'julie@localhost.com',
+        },
+      ],
+    };
+  },
+};
+</script>
