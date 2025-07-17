7a3f0de48a3658cb93399c3ed940a6307411f4df
Nguyen.Phuoc.Sang <phuocsang1902@gmail.com>
2025-07-17 11:09:42 +0700

    Add loadExperiences method to UserExperiences.vue for fetching and displaying submitted survey results. Update button to trigger data loading.

--- http-01-starting-setup/src/components/survey/UserExperiences.vue
+++ http-01-starting-setup/src/components/survey/UserExperiences.vue
@@ -3,7 +3,9 @@
     <base-card>
       <h2>Submitted Experiences</h2>
       <div>
-        <base-button>Load Submitted Experiences</base-button>
+        <base-button @click="loadExperiences"
+          >Load Submitted Experiences</base-button
+        >
       </div>
       <ul>
         <survey-result
@@ -21,9 +23,40 @@
 import SurveyResult from './SurveyResult.vue';
 
 export default {
-  props: ['results'],
   components: {
     SurveyResult,
+  },
+  data() {
+    return {
+      results: [],
+    };
+  },
+  methods: {
+    loadExperiences() {
+      fetch(
+        'https://vue-http-demo-b1c68-default-rtdb.asia-southeast1.firebasedatabase.app/surveys.json'
+      )
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          } else {
+            return response.json().then((errorData) => {
+              console.log(errorData);
+            });
+          }
+        })
+        .then((data) => {
+          const results = [];
+          for (const key in data) {
+            results.push({
+              id: key,
+              name: data[key].name,
+              rating: data[key].rating,
+            });
+          }
+          this.results = results;
+        });
+    },
   },
 };
 </script>
