8c7ab85fa01b118ea245860552b1d6cc7b55aed1
Nguyen.Phuoc.Sang <phuocsang1902@gmail.com>
2025-07-22 17:00:19 +0700

    Add Vuex store setup and integrate with App.vue
    - Introduced Vuex for state management by creating a store with a counter state.
    - Updated App.vue to display the counter and added a button for interaction.
    - Modified package.json to include Vuex as a dependency.
    - Created package-lock.json to lock dependencies.

--- vuex-01-starting-setup/src/App.vue
+++ vuex-01-starting-setup/src/App.vue
@@ -1,5 +1,9 @@
 <template>
-  <base-container title="Vuex"></base-container>
+  <base-container title="Vuex">
+    <h3>{{ $store.state.counter }}</h3>
+    <button>Add 1</button>
+  </base-container>
+  >
 </template>
 
 <script>
