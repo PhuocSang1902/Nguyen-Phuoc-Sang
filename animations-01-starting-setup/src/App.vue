80cab6648c999961ffdcc5463b82aff49f7b4233
Nguyen.Phuoc.Sang <phuocsang1902@gmail.com>
2025-07-22 15:58:31 +0700

    Enhance App.vue by adding user visibility toggle functionality with fade transitions. Implement before-enter hook for animations and refine modal display logic. Update styles for improved animation effects.

--- animations-01-starting-setup/src/App.vue
+++ animations-01-starting-setup/src/App.vue
@@ -4,15 +4,25 @@
     <button @click="animateBlock">Animate</button>
   </div>
   <div class="container">
-    <transition>
+    <transition @before-enter="beforeEnter">
       <p v-if="paragraphIsVisible">This is only sometimes visible...</p>
     </transition>
     <button @click="toggleParagraph">Toggle Paragraph</button>
   </div>
-    <base-modal @close="hideDialog" v-if="dialogIsVisible" :open="dialogIsVisible">
-      <p>This is a test dialog!</p>
-      <button @click="hideDialog">Close it!</button>
-    </base-modal>
+  <div class="container">
+    <transition name="fade-button" mode="out-in">
+      <button @click="showUsers" v-if="!usersAreVisible">Show Users</button>
+      <button @click="hideUsers" v-else>Hide Users</button>
+    </transition>
+  </div>
+  <base-modal
+    @close="hideDialog"
+    v-if="dialogIsVisible"
+    :open="dialogIsVisible"
+  >
+    <p>This is a test dialog!</p>
+    <button @click="hideDialog">Close it!</button>
+  </base-modal>
   <div class="container">
     <button @click="showDialog">Show Dialog</button>
   </div>
@@ -25,9 +35,19 @@ export default {
       dialogIsVisible: false,
       animatedBlock: false,
       paragraphIsVisible: false,
+      usersAreVisible: false,
     };
   },
   methods: {
+    beforeEnter(){
+      console.log('beforeEnter')
+    },
+    showUsers() {
+      this.usersAreVisible = true;
+    },
+    hideUsers() {
+      this.usersAreVisible = false;
+    },
     showDialog() {
       this.dialogIsVisible = true;
     },
@@ -118,6 +138,24 @@ button:active {
 
 .v-leave-active {
   transition: all 0.3s ease-out;
+}
+
+.fade-button-enter-from,
+.fade-button-leave-to {
+  opacity: 0;
+}
+
+.fade-button-enter-active {
+  transition: opacity 0.3s ease-out;
+}
+
+.fade-button-leave-active {
+  transition: opacity 0.3s ease-in;
+}
+
+.fade-button-enter-to,
+.fade-button-leave-from {
+  opacity: 1;
 }
 
 @keyframes slide-scale {
