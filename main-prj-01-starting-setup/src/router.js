416994754faba98eaee3192bb2a6e8bb7a55671d
Nguyen.Phuoc.Sang <phuocsang1902@gmail.com>
2025-07-24 16:58:19 +0700

    Add initial pages and router configuration for coaches and requests
    - Created App.vue as the main application component.
    - Defined routes in router.js for CoachesList, CoachDetails, CoachRegistration, ContactCoach, RequestsReceived, and NotFound pages.
    - Added new Vue components for each page to enhance application structure and navigation.

--- main-prj-01-starting-setup/src/router.js
+++ main-prj-01-starting-setup/src/router.js
@@ -1,18 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
+import CoachesList from './pages/coaches/CoachesList.vue';
+import CoachDetails from './pages/coaches/CoachDetails.vue';
+import CoachRegistration from './pages/coaches/CoachRegistration.vue';
+import ContactCoach from './pages/requests/ContactCoach.vue';
+import NotFound from './pages/NotFound.vue';
+import RequestsReceived from './pages/requests/RequestsReceived.vue';
+
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', redirect: '/coaches' },
-    { path: '/coaches', component: null },
+    { path: '/coaches', component: CoachesList },
     {
       path: '/coaches/:id',
-      component: null,
-      children: [{ path: 'contact', component: null }],
+      component: CoachDetails,
+      children: [{ path: 'contact', component: ContactCoach }],
     },
-    { path: '/register', component: null },
-    { path: '/contact', component: null },
-    { path: '/:notFound(.*)*', component: null },
+    { path: '/register', component: CoachRegistration },
+    { path: '/requests', component: RequestsReceived },
+    { path: '/:notFound(.*)*', component: NotFound },
   ],
 });
 
