617389028d57bce18de641d8c80431f7c3e1e94d
Nguyen.Phuoc.Sang <phuocsang1902@gmail.com>
2025-07-28 16:06:11 +0700

    Implement auto-login and route protection for authentication
    - Added auto-login functionality in App.vue to check user authentication status on app creation.
    - Enhanced router.js to protect routes requiring authentication and redirect unauthenticated users to the login page.
    - Refactored Vuex store actions for login and signup to streamline authentication processes.
    - Introduced new state management for auto logout and updated related mutations and getters in the auth module.

--- main-prj-01-starting-setup/src/store/modules/auth/actions.js
+++ main-prj-01-starting-setup/src/store/modules/auth/actions.js
@@ -1,6 +1,28 @@
+let timer;
 export default {
   async login(context, payload) {
-    const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCN5fFP-RyLsbhT0wa5HbGX4xDgBd-D65I', {
+    context.dispatch('auth', {
+      ...payload,
+      mode: 'login',
+    });
+  },
+  async signup(context, payload) {
+    context.dispatch('auth', {
+      ...payload,
+      mode: 'signup',
+    });
+  },
+  async auth(context, payload) {
+    const mode = payload.mode;
+    let url =
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCN5fFP-RyLsbhT0wa5HbGX4xDgBd-D65I';
+
+    if (mode === 'signup') {
+      url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCN5fFP-RyLsbhT0wa5HbGX4xDgBd-D65I';
+    }
+
+    const response = await fetch(url, {
       method: 'POST',
       body: JSON.stringify({
         email: payload.email,
@@ -13,37 +35,63 @@ export default {
       const error = new Error(responseData.error.message || 'Failed to login.');
       throw error;
     }
+
+    const expiresIn = responseData.expiresIn * 1000;
+
+    const expirationDate = new Date().getTime() + expiresIn;
+
+    localStorage.setItem('token', responseData.idToken);
+    localStorage.setItem('userId', responseData.localId);
+    localStorage.setItem('tokenExpiration', expirationDate);
+
+    timer = setTimeout(() => {
+      context.dispatch('autoLogout');
+    }, expiresIn);
+
     context.commit('setUser', {
       userId: responseData.localId,
       token: responseData.idToken,
-      tokenExpiration: responseData.expiresIn,
     });
   },
-  async signup(context, payload) {
-    const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCN5fFP-RyLsbhT0wa5HbGX4xDgBd-D65I', {
-      method: 'POST',
-      body: JSON.stringify({
-        email: payload.email,
-        password: payload.password,
-        returnSecureToken: true,
-      }),
-    });
-    const responseData = await response.json();
-    if (!response.ok) {
-      const error = new Error(responseData.error.message || 'Failed to sign up.');
-      throw error;
+  tryLogin(context) {
+    const token = localStorage.getItem('token');
+    const userId = localStorage.getItem('userId');
+    const tokenExpiration = localStorage.getItem('tokenExpiration');
+
+    const expiresIn = +tokenExpiration - new Date().getTime();
+
+    if (expiresIn < 0) {
+      return;
     }
-    context.commit('setUser', {
-      userId: responseData.localId,
-      token: responseData.idToken,
-      tokenExpiration: responseData.expiresIn,
-    });
+
+    timer = setTimeout(() => {
+      context.dispatch('autoLogout');
+    }, expiresIn);
+
+    if (token || userId) {
+      context.commit('setUser', {
+        userId: userId,
+        token: token,
+      });
+    }
   },
   async logout(context) {
+    localStorage.removeItem('token');
+    localStorage.removeItem('userId');
+    localStorage.removeItem('tokenExpiration');
+
+    if (timer) {
+      clearTimeout(timer);
+    }
+
     context.commit('setUser', {
       userId: null,
       token: null,
-      tokenExpiration: null,
     });
+  },
+  autoLogout(context) {
+    context.dispatch('logout');
+
+    context.commit('didAutoLogout');
   },
 };
